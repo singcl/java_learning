88b09864076457464c38f6696ee49b4ab5059c37
singcl <24661881@qq.com>
2019-06-22 16:08:19 +0800

    :art: 简单的java package and String[]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import com.HelloWorld.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        String[] x = {"设么鬼"};
+        String[] xx = new String[]{"设么鬼"};
+        HelloWorld.main(xx);
     }
 }
---
+++ src/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.HelloWorld;
+
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print(Arrays.toString(args));
+        System.out.println("\nHello World!");
+    }
+}
