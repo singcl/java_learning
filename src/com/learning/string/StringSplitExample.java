a29be0321bf1045e6ad8e62bfd82c492c0c7d3d7
singcl <24661881@qq.com>
2019-07-10 23:57:48 +0800

    :art: 字符串基本操作 - StringTokenizer

--- src/com/learning/string/StringSplitExample.java
+++ src/com/learning/string/StringSplitExample.java
@@ -27,6 +27,16 @@ public class StringSplitExample {
 
         String  str2 = "This is String , split by StringTokenizer, created by runoob";
         StringTokenizer st = new StringTokenizer(str2);
+
         System.out.println("----- 通过空格分隔 ------");
+        while (st.hasMoreElements()) {
+            System.out.println(st.nextElement());
+        }
+
+        System.out.println("----- 通过逗号分隔 ------");
+        StringTokenizer st2 = new StringTokenizer(str2, ",");
+        while (st2.hasMoreElements()) {
+            System.out.println(st2.nextElement());
+        }
     }
 }
