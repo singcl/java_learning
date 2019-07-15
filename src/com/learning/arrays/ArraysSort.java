e33a0f73c65ca28625055871f37c9b1384c89dbb
singcl <24661881@qq.com>
2019-07-15 22:19:12 +0800

    :art: 字符串基本操作 - Arrays.sort()

---
+++ src/com/learning/arrays/ArraysSort.java
@@ -0,0 +1,24 @@
+package com.learning.arrays;
+
+
+import java.util.Arrays;
+public class ArraysSort {
+    public static void main(String[] args) throws Exception {
+        int array[] = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
+        Arrays.sort(array);
+        printArray("数组排序结果", array);
+        int index = Arrays.binarySearch(array, 2);
+        System.out.println("元素 2  在第 " + index + " 个位置");
+    }
+
+    public static void printArray(String message,int array[]) {
+        System.out.print(message + ": [length: " + array.length + "]");
+        for ( int i = 0; i < array.length; i++) {
+            if (i != 0 ) {
+                System.out.print(",");
+            }
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+}
