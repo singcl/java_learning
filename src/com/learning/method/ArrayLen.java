d1d9881a1a6e2f5535218603bccc7bdf58f06cfd
singcl <1276837689@qq.com>
2019-07-29 23:00:56 +0800

    :art: 数组长度

---
+++ src/com/learning/method/ArrayLen.java
@@ -0,0 +1,9 @@
+package com.learning.method;
+
+public class ArrayLen {
+    public static void main(String[] args) {
+        String[][] data = new String[2][5];
+        System.out.println("第一维数组的长度：" + data.length);
+        System.out.println("第二维数组的长度：" + data[0].length);
+    }
+}
