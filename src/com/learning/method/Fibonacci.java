0eb00602810c2582ef6f7a8c806fc353e5c54b5a
singcl <1276837689@qq.com>
2019-09-10 15:35:32 +0800

    :art: fibonacci 递归求值

---
+++ src/com/learning/method/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.learning.method;
+
+public class Fibonacci {
+    public static long fibonacci(long number) {
+        if ((number == 0) || (number == 1)) {
+            return number;
+        } else {
+            return fibonacci(number -1) + fibonacci(number - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int counter = 0; counter <= 10; counter++) {
+            System.out.printf("Fibonacci of %d is : %d", counter, fibonacci(counter));
+        }
+    }
+}
